f28fd66340b8cfbfdaff6f2161f160cdb1994571
dennisblessed2 <dennismulwa418@gmail.com>
2021-02-23 06:57:19 +0300

    add changes to index.html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const getGender = () => {
+    let male = document.getElementById("male").value
+    let female = document.getElementById("female").value
+    var getGender;
+    if (gender == "Male") {
+        document.getElementById
+    }
+}
+console.log(male, female)
+const dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+console.log(daysOfTheWeek)
+const maleNames = ["kwasi", "kwandwoa", "kwabenaa", "kwaku", "yaw", "kofi", "kwame"]
+const femaleNames=[ ]
