1996508167bb010688dfc02f7a30a9c99025c79e
dennisblessed2 <dennismulwa418@gmail.com>
2021-02-23 15:11:30 +0300

    add changes to index.html

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-const getGender = () => {
-    let male = document.getElementById("male").value
-    let female = document.getElementById("female").value
-}
+
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const maleNames = ["kwasi", "kwandwoa", "kwabenaa", "kwaku", "yaw", "kofi", "kwame"];
 const femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","AMa" ];
 
 const calculateDay = () => {
-    year = document.getElementById("year").value
+    event.preventDefault()
+    onst getGender = () => {
+        let male = document.getElementById("male").value
+    
+        let female = document.getElementById("female").value
+    
+   const year = document.getElementById("year").value
     const CC = parseInt[year.substring(0 , 2)];
     const YY = parseInt[year.substring(2 ,4)];
     const MM = parseInt(document.getElementById["Month"].value);
     const DD = parseInt(document.getElementById["date"].value);
+    console.log(year)
     const d = Math.round( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 
-}
+    console.log[d];
+    return d;
+
+    }
