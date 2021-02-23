3f44ddf515893d0e67d329ff0324447dd57a9b62
dennisblessed2 <dennismulwa418@gmail.com>
2021-02-23 13:48:52 +0300

    add changes to index.html

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ const femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","AMa" ];
 
 const calculateDay = () => {
     year = document.getElementById("year").value
-    const CC = parseInt(year.substring(0 , 2));
-    const YY = parseInt(year.substring(2 ,4));
+    const CC = parseInt[year.substring(0 , 2)];
+    const YY = parseInt[year.substring(2 ,4)];
+    const MM = parseInt(document.getElementById["Month"].value);
+    const DD = parseInt(document.getElementById["date"].value);
+    const d = Math.round( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 
 }
