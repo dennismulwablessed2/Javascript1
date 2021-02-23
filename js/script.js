02691ec37a9de09a9a24abaf458f781bc156abca
dennisblessed2 <dennismulwa418@gmail.com>
2021-02-23 12:22:50 +0300

    add js/script.js to index.html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 const getGender = () => {
     let male = document.getElementById("male").value
     let female = document.getElementById("female").value
-    var getGender;
-    if (gender == "Male") {
-        document.getElementById
-    }
 }
-console.log(male, female)
-const dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-console.log(daysOfTheWeek)
-const maleNames = ["kwasi", "kwandwoa", "kwabenaa", "kwaku", "yaw", "kofi", "kwame"]
-const femaleNames=[ ]
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const maleNames = ["kwasi", "kwandwoa", "kwabenaa", "kwaku", "yaw", "kofi", "kwame"];
+const femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","AMa" ];
+
+const calculateDay
