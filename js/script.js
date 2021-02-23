18eab5eb493a69f3ebdd8f6382fb638789a4c181
dennisblessed2 <dennismulwa418@gmail.com>
2021-02-23 16:16:03 +0300

    add changes to index.html

--- js/script.js
+++ js/script.js
@@ -2,22 +2,26 @@
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const maleNames = ["kwasi", "kwandwoa", "kwabenaa", "kwaku", "yaw", "kofi", "kwame"];
 const femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","AMa" ];
-
+const months=["January","February","March","April","May","June","July","August","September","October","November","December"]
 const calculateDay = () => {
     event.preventDefault()
-    onst getGender = () => {
-        let male = document.getElementById("male").value
-    
-        let female = document.getElementById("female").value
-    
+
    const year = document.getElementById("year").value
     const CC = parseInt[year.substring(0 , 2)];
     const YY = parseInt[year.substring(2 ,4)];
-    const MM = parseInt(document.getElementById["Month"].value);
+    const MM = parseInt(document.getElementById["month"].value);
     const DD = parseInt(document.getElementById["date"].value);
     console.log(year)
     const d = Math.round( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 
     console.log[d];
     return d;
 
-    }
+};
+const getGender = () => {
+  const maleName = document.getElementById("male"); 
+   const femaleName= document.getElementById("female");
+  
+      
+  }
+
+       
