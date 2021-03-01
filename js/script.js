7886e6dbc04f28d1a9de505ab143d4898dbc34ac
dennisblessed2 <dennismulwa418@gmail.com>
2021-03-01 14:29:50 +0300

    add changes made in js/script.js to index.html

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
-
-const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const year = document.getElementById("year").value
+    const CC = parseInt[year.substring(0 , 2)];
+    const YY = parseInt[year.substring(2 ,4)];
+let dayOfTheWeek = daysOfTheWeek[dayBorn]
 const maleNames = ["kwasi", "kwandwoa", "kwabenaa", "kwaku", "yaw", "kofi", "kwame"];
 const femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","AMa" ];
 const month=["January","February","March","April","May","June","July","August","September","October","November","December"]
-const calculateDay = () => {
-    event.preventDefault()
+if(gender === "male"&& dayOfTheWeek === "Sunday"){
+  console.log(male[dayBorn],"kwasi");
+  elseif(gender === "male"&& dayOfTheWeek === "Monday")
+    console.log(male[dayBorn],"kwandwoa");
+    elseif(gender === "male"&& dayOfTheWeek === "Tuesday")
+    console.log(male[dayBorn],"kwabenaa");
+    elseif(gender === "male"&& dayOfTheWeek === "Wednesday")
+    console.log(male[dayBorn],"kwaku");
+    elseif(gender === "male"&& dayOfTheWeek === "Thursday")
+    console.log(male[dayBorn],"yaw");
+    elseif(gender === "male"&& dayOfTheWeek === "Friday")
+    console.log(male[dayBorn],"kofi");
+    elseif(gender === "male"&& dayOfTheWeek === "Saturday")
+    console.log(male[dayBorn],"kwame");
+}
+if(gender === "female"&& dayOfTheWeek === "Sunday"){
+  console.log(female[dayBorn],"Akosua");
+  elseif(gender === "female"&& dayOfTheWeek === "Monday")
+    console.log(female[dayBorn],"Adwoa");
+    elseif(gender === "female"&& dayOfTheWeek === "Tuesday")
+    console.log(female[dayBorn],"Abenaa");
+    elseif(gender === "female"&& dayOfTheWeek === "Wednesday")
+    console.log(female[dayBorn],"Akua");
+    elseif(gender === "female"&& dayOfTheWeek === "Thursday")
+    console.log(female[dayBorn],"Yaa");
+    elseif(gender === "female"&& dayOfTheWeek === "Friday")
+    console.log(female[dayBorn],"Afua");
+    elseif(gender === "female"&& dayOfTheWeek === "Saturday")
+    console.log(male[dayBorn],"Ama");
+}
 
-   const year = document.getElementById("year").value
-    const CC = parseInt[year.substring(0 , 2)];
-    const YY = parseInt[year.substring(2 ,4)];
-    const MM = parseInt(document.getElementById["month"].value);
-    const DD = parseInt(document.getElementById["date"].value);
-    console.log(year)
-    const daysOfTheWeek= Math.round( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 
-    console.log[dd];
-    return d;
-
-};
-const getGender = () => {
-  const maleName = document.getElementById("male"); 
-   const femaleName= document.getElementById("female");
-  console.log(male,female)
-      
-  }
 
        
